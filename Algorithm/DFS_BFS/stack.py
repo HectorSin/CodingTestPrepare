daa484fd24df95b58ea5c8d46159fbae974211ff
HectorSin <kkang15634@ajou.ac.kr>
2025-12-19 08:37:23 +0900

    동기화

---
+++ Algorithm/DFS_BFS/stack.py
@@ -0,0 +1,13 @@
+stack = []
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack)
+print(stack[::-1])  # 스택은 LIFO이므로 역순으로 출력   
